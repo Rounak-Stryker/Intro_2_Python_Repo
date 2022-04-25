0c6cdd9cab7df75e50a825a9a3272488af40fd65
Rounak-Stryker <95473993+Rounak-Stryker@users.noreply.github.com>
2022-04-25 12:19:16 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Rounak Chatterjee
+#
+# Created:     12-04-2022
+# Copyright:   (c) Rounak Chatterjee 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first_number=float(input("first number = "))
+second_number=float(input("second number = "))
+third_number=float(input("third number = "))
+average=(first_number + second_number + third_number)/3
+print("Average =",average)
